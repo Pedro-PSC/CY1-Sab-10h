002105fd2f8b2c4ac2726cfeb4b5bdb23992563d
Pedro Paulo <pedropsc100@gmail.com>
2025-09-27 12:01:25 -0300

    Aula08

---
+++ Aula08/modulos.py
@@ -0,0 +1,13 @@
+import math
+#from matematica.calculadora import soma as s
+import matematica.calculadora
+from matematica import calculadora
+from matematica.calculadora import soma as so , subtracao as sub
+
+print(dir(math))
+print(help(math.floor))
+
+print(calculadora.soma(2, 3))
+print(help(calculadora.soma))
+
+print(so(3, 2))
